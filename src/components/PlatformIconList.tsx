d26c38b81c885fbb7d970eb2deb49c080176a036
Aiman Afiq <aimanafiq05@gmail.com>
2023-07-29 23:18:34 +0800

    Fix weird typescript error

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -17,26 +17,22 @@ interface Props {
   platforms: Platform[];
 }
 
-interface IconMap {
-  [key: string]: IconType;
-}
-
 function PlatformIconList({ platforms = [] }: Props) {
-  const iconMap: IconMap = {
-    pc: FaWindows,
-    playstation: FaPlaystation,
-    xbox: FaXbox,
-    nintendo: SiNintendo,
-    mac: FaApple,
-    linux: FaLinux,
-    android: FaAndroid,
-    ios: MdPhoneIphone,
-    web: BsGlobe,
-  };
+  const iconMap = new Map<string, IconType>([
+    ["pc", FaWindows],
+    ["playstation", FaPlaystation],
+    ["xbox", FaXbox],
+    ["nintendo", SiNintendo],
+    ["mac", FaApple],
+    ["linux", FaLinux],
+    ["android", FaAndroid],
+    ["ios", MdPhoneIphone],
+    ["web", BsGlobe],
+  ]);
   return (
     <HStack marginY={"1em"}>
       {platforms.map((platform) => (
-        <Icon as={iconMap[platform.slug]} />
+        <Icon as={iconMap.get(platform.slug)} />
       ))}
     </HStack>
   );
