795ca084772afac57a0287add893676fe3df9a89
Aiman Afiq <aimanafiq05@gmail.com>
2023-07-29 16:35:05 +0800

    Added basic responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+
 function App() {
-  return <></>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="orange">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="green">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="blue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
