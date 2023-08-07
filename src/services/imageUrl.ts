dd2da37b0f2f5e24e591db379d3c1ae6aa9d2d75
Aiman Afiq <aimanafiq05@gmail.com>
2023-08-07 23:42:59 +0800

    Close guard, add no image placeholder

--- src/services/imageUrl.ts
+++ src/services/imageUrl.ts
@@ -1,9 +1,9 @@
+import noImage from "../assets/no-image-placeholder-6f3882e0.webp";
+
 const getCroppedImageUrl = function (url: string) {
-  if (url) {
-    const target = "media/";
-    const headerIndex = url.indexOf(target) + target.length;
-    return url.slice(0, headerIndex) + "crop/600/400/" + url.slice(headerIndex);
-  }
-  return "";
+  if (!url) return noImage;
+  const target = "media/";
+  const headerIndex = url.indexOf(target) + target.length;
+  return url.slice(0, headerIndex) + "crop/600/400/" + url.slice(headerIndex);
 };
 export default getCroppedImageUrl;
