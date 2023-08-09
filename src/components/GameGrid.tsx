be82c6b0a2fc912a13bd903a46dc0fc8cb102a80
Aiman Afiq <aimanafiq05@gmail.com>
2023-08-09 22:17:16 +0800

    eeeee

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,9 +12,9 @@ interface Props {
 function GameGrid({ gameQuery }: Props) {
   const { games, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
+  if (error) return <Text>{error}</Text>;
   return (
     <>
-      {error && <Text>{error}</Text>}
       <Grid
         templateColumns={{
           sm: "1fr",
